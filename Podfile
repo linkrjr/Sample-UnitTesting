38ec6aff43d798274c072ce019f013349d7eb9d4
Ronaldo Gomes <ronaldo.junior@gmail.com>
2016-04-01 17:17:10 +1100

    Initial commit

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+def testing_pods
+  pod 'Quick', '~> 0.9.0'
+  pod 'Nimble', '~> 3.2.0'   
+  pod 'SwiftMock'
+end
+
+target 'Sample-UnitTesting' do
+
+end
+
+target 'Sample-UnitTestingTests' do
+  testing_pods
+end
+
+target 'Sample-UnitTestingUITests' do
+  testing_pods
+end
+
---
+++ Sample-UnitTestingTests/Sample_UnitTestingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sample_UnitTestingTests.swift
+//  Sample-UnitTestingTests
+//
+//  Created by Ronaldo GomesJr on 1/04/2016.
+//  Copyright © 2016 Technophile IT. All rights reserved.
+//
+
+import XCTest
+@testable import Sample_UnitTesting
+
+class Sample_UnitTestingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
